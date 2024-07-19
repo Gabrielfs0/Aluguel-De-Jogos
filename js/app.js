b226683860487d98accfc48edc2fa12f54617896
Gabriel Franco <gf12166@gmail.com>
2024-07-19 19:14:21 -0300

    Atualizando Projeto

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 let contadorAlugados = 0
 
+// Exibe no console o total de jogos alugados
 function contarEExibirJogosAlugados() {
   console.log(`Total de jogos alugados: ${jogosAlugados}`);
 }
@@ -15,24 +16,26 @@ function alterarStatus(id) {
     // Trocar o botão, texto e imagem do jogo de acordo com o jogo, alugando ou devolvendo
     if (imagem.classList.contains("dashboard__item__img--rented")) {
 
-      // Adiciona uma confirmação antes de devolver o jogo
+      // Adiciona uma CONFIRMÇÂO PARA O USUARIO antes de devolver o jogo
       if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
          imagem.classList.remove("dashboard__item__img--rented");
          botao.classList.remove("dashboard__item__button--return");
          botao.innerHTML = "Alugar"
+         // Diminuir -1 ao contador de acordo com os jogos alugados do usuario
          jogosAlugados--;
       }
   } else {
       imagem.classList.add("dashboard__item__img--rented");
       botao.classList.add("dashboard__item__button--return");  
       botao.innerHTML = "Devolver" 
+     // Acrecentar +1 ao contador de acordo com os jogos alugados do usuario
       jogosAlugados++;
     }
-
+    // Exibe a Função
     contarEExibirJogosAlugados();
 }
 
-// Inicializa a contagem considerando que os jogos já começam alugados
+// 'DOMContentLoaded'É acionado quando a página é carregada / Conta a quantidade inicial de jogos já alugados e inicia a contagem diante disso
 document.addEventListener('DOMContentLoaded', function() {
   jogosAlugados = document.querySelectorAll('.dashboard__item__img--rented').length;
   contarEExibirJogosAlugados();
